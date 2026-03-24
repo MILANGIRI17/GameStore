4eaa968d871ee48f162142f9ee577400bd97f323
milan giri <milan.giri17021999@gmail.com>
2026-03-24 21:26:09 +0545

    Created a Dto

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -2,8 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 //GET /games
-app.MapGet("/games", () => {
+app.MapGet("/games", () => 
+{
     return "Hello World!";
-  });
+});
 
 app.Run();
