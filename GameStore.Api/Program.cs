82f087292598dcb76573e8a7380b9f98a5a3f9dd
Milan_Giri <milan@teksewa.com>
2026-03-26 16:35:59 +0545

    Adding input validation

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,10 @@
 using GameStore.Api.EndPoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//Services
+builder.Services.AddValidation();
+
 var app = builder.Build();
 app.MapGamesEndpoints();
 app.Run();
