93cb507c4e6351d9a87a9fd48b20bc7d411ca027
Milan_Giri <milan@teksewa.com>
2026-03-26 16:24:04 +0545

    Refactoring games endpoints

---
+++ GameStore.Api/EndPoints/GamesEndpoints.cs
@@ -0,0 +1,61 @@
+using GameStore.Api.Dtos;
+
+namespace GameStore.Api.EndPoints;
+
+public static class GamesEndpoints
+{
+    const string GetGameEnpointName = "GetGame";
+    private static readonly List<GameDto> games =
+    [
+        new(1, "Street Fighter II", "Fighting", 19.99m, new DateOnly(1992, 7, 15)),
+        new(2, "Final Fantasy VII Rebirth", "RPG", 69.99m, new DateOnly(2024, 2, 29)),
+        new(3, "Astro Bot", "Platformer", 59.99m, new DateOnly(2024, 9, 6)),
+    ];
+
+    public static void MapGamesEndpoints(this WebApplication app)
+    {
+        //GET /games
+        app.MapGet("/games", () => games);
+
+        //GET /games/1
+        app.MapGet("/games/{id}", (int id) =>
+        {
+            var game = games.Find(game => game.Id == id);
+            return game is null ? Results.NotFound() : Results.Ok(game);
+        }).WithName(GetGameEnpointName);
+
+        //POST /games
+        app.MapPost("/games", (CreateGameDto newGame) =>
+        {
+            GameDto game = new(games.Count + 1, newGame.Name, newGame.Genre, newGame.Price, newGame.ReleaseDate);
+            games.Add(game);
+            return Results.CreatedAtRoute(GetGameEnpointName, new { id = game.Id }, game);
+        });
+
+        //Update /games/1
+        app.MapPut("/games/{id}", (int id, UpdateGameDto updateGame) =>
+        {
+            var index = games.FindIndex(g => g.Id == id);
+
+            if (index == -1) return Results.NotFound();
+
+            games[index] = new GameDto(
+                id,
+                updateGame.Name,
+                updateGame.Genre,
+                updateGame.Price,
+                updateGame.ReleaseDate
+            );
+            return Results.NoContent();
+        });
+
+        //Delete /games/1
+        app.MapDelete("/games/{id}", (int id) =>
+        {
+            var index = games.FindIndex(game => game.Id == id);
+            if (index == -1) return Results.NotFound("Game not found");
+            games.RemoveAt(index);
+            return Results.NoContent();
+        });
+    }
+}
