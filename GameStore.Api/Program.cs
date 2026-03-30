1892b5ae9d951213bd65a9de363b2d0c04a04e4c
milan giri <milan.giri17021999@gmail.com>
2026-03-30 22:46:04 +0545

    Added an extension method for database logic

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,29 +1,10 @@
 using GameStore.Api.Data;
 using GameStore.Api.EndPoints;
-using GameStore.Api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Database
-var connectionString = "Data Source=GameStore.db";
-builder.Services.AddSqlite<GameStoreContext>(
-    connectionString,
-    optionsAction: options => options.UseSeeding((context, _) =>
-    {
-        if (!context.Set<Genre>().Any())
-        {
-            context.Set<Genre>()
-            .AddRange
-            (
-                new Genre{ Name = "Fighting"},
-                new Genre{ Name = "RPG"},
-                new Genre{ Name = "Platformer"},
-                new Genre{ Name = "Racing"},
-                new Genre{ Name = "Sports"}
-            );
-            context.SaveChanges();
-        }
-    }));
+builder.AddGameStoreDb();
 
 //Services
 builder.Services.AddValidation();
