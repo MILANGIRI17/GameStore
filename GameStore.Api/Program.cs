3fbcd0324d38a8ef68d9faf73191ec8821f59357
milan giri <milan.giri17021999@gmail.com>
2026-03-30 22:40:56 +0545

    Dataseed for Genre table

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,11 +1,29 @@
 using GameStore.Api.Data;
 using GameStore.Api.EndPoints;
+using GameStore.Api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Database
 var connectionString = "Data Source=GameStore.db";
-builder.Services.AddSqlite<GameStoreContext>(connectionString);
+builder.Services.AddSqlite<GameStoreContext>(
+    connectionString,
+    optionsAction: options => options.UseSeeding((context, _) =>
+    {
+        if (!context.Set<Genre>().Any())
+        {
+            context.Set<Genre>()
+            .AddRange
+            (
+                new Genre{ Name = "Fighting"},
+                new Genre{ Name = "RPG"},
+                new Genre{ Name = "Platformer"},
+                new Genre{ Name = "Racing"},
+                new Genre{ Name = "Sports"}
+            );
+            context.SaveChanges();
+        }
+    }));
 
 //Services
 builder.Services.AddValidation();
